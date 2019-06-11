e3db8adf30aaf2478576ea26a40b79965d3bdd11
Olivia Sandvold <a9paxzz@mmm.com>
2019-06-11 13:39:50 -0500

    When button press, send message to topic

--- pi/connectAWSiot.py
+++ pi/connectAWSiot.py
@@ -4,10 +4,15 @@ import time
 import argparse
 import json
 
+# set up Pi pins
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# AWS
 host = "a15llkyp1ib6yl-ats.iot.us-east-1.amazonaws.com"
 certPath = "/home/pi/certificates/"
 clientId = "osandvold"
-topic = "demo-topic"
+topic = "online-post-it"
 
 # Init AWSIoTMQTTClient
 myAWSIoTMQTTClient = None
@@ -23,15 +28,14 @@ myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
 myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
 myAWSIoTMQTTClient.connect()
 
-# Publish to the same topic in a loop forever
-loopCount = 0
 while True:
     message = {}
-    message['message'] = "demo-topic-sample-message"
-    message['sequence'] = loopCount
-    messageJson = json.dumps(message)
-    myAWSIoTMQTTClient.publish(topic, messageJson, 1)
-    print('Published topic %s: %s\n' % (topic, messageJson))
-    loopCount += 1
-    time.sleep(10)
+    input_state = GPIO.input(18)
+    if input_state == False:
+        message['message'] = "pressed button"
+        messageJson = json.dumps(message)
+        myAWSIoTMQTTClient.publish(topic, messageJson, 1)
+        print('Published topic %s: %s\n' % (topic, messageJson))
+        time.sleep(10)  # TODO: Set to 60 for real example
+
 myAWSIoTMQTTClient.disconnect()
